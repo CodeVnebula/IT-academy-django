9d513df8af3f0bcabc2c5fb840532ece4b893cbc
CodeVnebula <chkhikvadzem471@gmail.com>
2024-10-04 23:45:42 +0400

    Add new apps to installed apps list

--- core/core/settings.py
+++ core/core/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'order.apps.OrderConfig',
+    'store.apps.StoreConfig',   
 ]
 
 MIDDLEWARE = [
